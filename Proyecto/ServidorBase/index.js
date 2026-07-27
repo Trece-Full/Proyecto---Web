df362c30cd568a7f95a9d65b7180b1b37dcdd7f1
Ronaldo <xdoooorrr@gmail.com>
2026-07-27 15:41:10 -0700

    Se cambio la ruta de index.html

--- Proyecto/ServidorBase/index.js
+++ Proyecto/ServidorBase/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "..")));
+app.use(express.static(path.join(__dirname, "../src")));
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
